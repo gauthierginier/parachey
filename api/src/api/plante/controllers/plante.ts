2017f2e2d20caf33acd1344ee4b4455cd2022568
gauthierg <gauthier.ginier@aleia.com>
2025-04-10 00:12:24 -0400

    feat(api): add strapi based api

---
+++ api/src/api/plante/controllers/plante.ts
@@ -0,0 +1,7 @@
+/**
+ * plante controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::plante.plante');
---
+++ api/src/api/plante/services/plante.ts
@@ -0,0 +1,7 @@
+/**
+ * plante service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::plante.plante');
